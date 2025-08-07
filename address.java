aebf1d14b3c6b57f5bb18a825d6bebb0674389c8
shivatomar-cloud <broshivathakur@gmail.com>
2025-08-07 15:51:47 +0530

    address.java

---
+++ address.java
@@ -0,0 +1,8 @@
+public class AddressMultiPrint {
+    public static void main(String[] args) {
+        System.out.println("Shiva Tomar");
+        System.out.println("123 Main Street");
+        System.out.println("Agra");
+        System.out.println("Uttar Pradesh - 282001");
+    }
+}
