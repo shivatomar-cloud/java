efa977230f4d958e40f45d986f333d4a9438496f
shivatomar-cloud <broshivathakur@gmail.com>
2025-08-14 12:21:48 +0530

    Sumdouble.java

---
+++ Sumdouble.java
@@ -0,0 +1,9 @@
+public class SumDouble {
+    public static int sumDouble(int a, int b) {
+        if (a == b) {
+            return 2 * (a + b);
+        } else {
+            return a + b;
+        }
+    }
+}
